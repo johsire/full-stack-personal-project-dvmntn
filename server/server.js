ba0d16cf3919e50adb66a1e929fcdeecc567de14
Josiah Maina <josiah5115@gmail.com>
2018-08-06 16:48:21 -0600

    setup session in the server

--- server/server.js
+++ server/server.js
@@ -16,6 +16,51 @@ const {
 } = process.env;
 
 
+massive(CONNECTION_STRING).then(db => {
+ app.set('db', db)
+});
+
+app.use(session({
+ secret: SESSION_SECRET,
+ resave: false,
+ saveUninitialized: false  
+}));
+
+
+app.get('/auth/callback', async (req, res) => {
+ let payload = {
+   client_id: REACT_APP_CLIENT_ID,
+   client_secret: CLIENT_SECRET,
+   code: req.query.code,
+   grant_type: 'authorization_code',
+   redirect_uri: `http://${req.headers.host}/auth/callback`
+ };
+
+ let resWithToken = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload);
+  
+
+  let resWithUserData = await axios.get(`https://${REACT_APP_DOMAIN}/userinfo?access_token=${resWithToken.data.access_token}`);
+
+  const db = req.app.get('db');
+  let {sub, email, name, picture} = resWithUserData.data;
+  let foundUser = await db.find_user([sub]);
+
+
+  if (foundUser[0]) {
+      req.session.user = foundUser[0];
+      res.redirect('/#shoppingcart');
+  } else {
+    let createdUser = await db.create_user([name, email, sub, picture]);
+    req.session.user = createdUser[0];
+    res.redirect('/#/');
+  };
+});
+
+
+app.get('/api/logout', (req, res) => {
+ req.session.destroy();
+ res.redirect('http://localhost:3000');
+});
 
 
 app.listen(SERVER_PORT, () => {
