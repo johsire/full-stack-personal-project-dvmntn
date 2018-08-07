3de6311ec60727b93c77aee7c72d9f200e97ac6d
Josiah Maina <josiah5115@gmail.com>
2018-08-07 14:00:36 -0600

    setup Nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import routes from './routes';
+import Nav from './components/nav/Nav';
 import LandingContainer from './containers/landing';
 import ProductContainer from './containers/product'
 
 const App = () => (
   <div>
-  <LandingContainer />
+   <LandingContainer />
    <ProductContainer />
+
+
+   <Nav />
+   { routes }
   </div>
 );
 
 export default App;
+ 
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
+function Nav() {
+ return (
+  <div>
+
+  </div>
+ );
+};
+
+export default Nav;
