1894ca8afebabd7879045f7221aa69bb8ed291bb
Josiah Maina <josiah5115@gmail.com>
2018-08-07 13:30:38 -0600

    produt-container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import LandingContainer from './containers/landing';
+import ProductContainer from './containers/product'
 
 const App = () => (
+  <div>
   <LandingContainer />
+   <ProductContainer />
+  </div>
 );
 
 export default App;
