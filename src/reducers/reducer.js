da601707126f808bc555efbc4b3faf23373fc5d0
Josiah Maina <josiah5115@gmail.com>
2018-08-06 20:21:59 -0600

    store fixed

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,25 +1,23 @@
 
-const initialState = {
- user: { },
+const initalState = {
+ user: {}
 };
 
-
-const UPDATE_USER_DATA = 'UPDATE_USER_DATA';
+const  UPDATE_USER_DATA = 'UPDATE_USER_DATA';
 
 export function updateUserData(user) {
  return {
-  type: UPDATE_USER_DATA,
-  payload: user
+   type: UPDATE_USER_DATA,
+   payload: user
  }
-};
+}
 
-export default function reducer(state = initialState, action) {
+export default function reducer(state = initalState, action) {
  switch (action.type) {
-  case ACTION_NAME:
-   return Object.assign({}, state, {
- 
-   });
-  default:
-   return state;
+   case UPDATE_USER_DATA:
+   return Object.assign({}, state, { user: action.payload });
+
+   default:
+     return state;
  }
 };
