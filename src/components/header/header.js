4b3a35a4b087002034d3b050f6f813fd72d3a6f5
Josiah Maina <josiah5115@gmail.com>
2018-08-08 01:23:12 -0600

    login & logout front-end

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,6 +9,13 @@ import { HeaderWrapper, Logo, Navigation, Menu } from './styles';
 let isLoggedIn = false;
 const authenticateAuth0 = () => {
   // logic goes here
+  isLoggedIn() {
+    let { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
+    let url = `${window.location.origin}/auth/callback`
+
+    window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
+  }
+
   console.log(isLoggedIn, 'LOGGED IN STATUS');
   console.log('tset');
   isLoggedIn = true;
--- src/containers/account/account-container.js
+++ src/containers/account/account-container.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Account from '../../components/account';
 
 class AccountContainer extends Component {
+  logout() {
+    axios.get('/api/logout').then(res => {
+      this.props.history.push('/');
+    });
+  };
+  
  render() {
    return (
     <Account title={"This is the Account page"} />
